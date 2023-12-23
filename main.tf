ea0146d954643ac89c7382acebc46095f6251909
Feleuxens <felixgrueb@icloud.com>
2023-12-24 00:10:09 +0100

    Create module

---
+++ main.tf
@@ -0,0 +1,22 @@
+resource "kubernetes_service_account" "ci_service_account" {
+    metadata {
+        name = var.serviceaccount_name
+    }   
+}
+
+resource "kubernetes_cluster_role_binding" "ci_cluster_role_binding" {
+  metadata {
+    name = "ci-cluster-role-binding"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind = "ClusterRole"
+    name = "cluster-admin"
+  }
+  subject {
+    kind = "ServiceAccount"
+    name = var.serviceaccount_name
+  }
+
+  depends_on = [ kubernetes_service_account.ci_service_account ]
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "serviceaccount_name" {
+  type = string
+  default = "ci-user"
+  description = "Name of the Service Account"
+}
